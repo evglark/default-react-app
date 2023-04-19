8147c6240e8dc5645b9fe67c96a976ee706483b6
Eugene Larkin <evglark@eugenes-macbook-pro.home>
2023-04-19 22:23:33 +0200

    Added json reader

--- src/module/JsonBuilder/components/JsonForm/JsonForm.tsx
+++ src/module/JsonBuilder/components/JsonForm/JsonForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { faClone } from '@fortawesome/free-regular-svg-icons';
+import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { Input } from '../Input';
@@ -8,18 +8,22 @@ import { ObjectBlock } from './ObjectBlock';
 import './style.scss';
 
 export const JsonForm = () => {
-    const [JSON, setJSON] = useState({ name: '' });
+    const [JSON, setJSON] = useState({
+        name: '',
+        obj: { a: 'AA', b: 'BB' },
+        obj1: { arr: ['AA', 'BB'] },
+        arr: [1, 2, 3],
+        arr1: [{ a: 'a', b: 'b' }],
+        arr2: [[1, 2, 3], [1, 2, 3]],
+    });
+
+    const resetJson = async () => {
+        const data = require('./users.json');
+        setJSON(data);
+    };
 
     const addNewField = () => {
-        setJSON((json) => {
-            const namesCount = Object.keys(json).filter(el => el.slice(0, 3)).length;
-
-            if (namesCount) {
-                return { ...json, ['name ' + (namesCount + 1).toString()]: '' };
-            }
-
-            return { ...json, name: '' };
-        });
+        console.log(1);
     };
 
     const changeName = () => {
@@ -27,38 +31,48 @@ export const JsonForm = () => {
     };
 
     const renderJson = () => {
-        const readObject = (json: any) =>
-            Object.keys(json).map((el, i) => {
+        const renderArray = (array: any[]) => {
+            return (
+                <div className="array-block">
+                    {array.map((arrayElement: any, i: number) => {
+                        if (Array.isArray(arrayElement)) {
+                            return <ObjectBlock el={'[]'}>{renderArray(arrayElement)}</ObjectBlock>;
+                        }
+
+                        if (typeof arrayElement === 'object') {
+                            return <ObjectBlock el={'{} ' + arrayElement}>{renderObject(arrayElement)}</ObjectBlock>;
+                        }
+
+                        return <Input value={arrayElement} onChangeName={changeName} key={arrayElement + i} />;
+                    })}
+                </div>
+            );
+        };
+
+        const renderObject = (json: any) => {
+            return Object.keys(json).map((el, i) => {
                 if (Array.isArray(json[el])) {
-                    return (
-                        <React.Fragment key={el+i}>
-                            <ObjectBlock el={el}>
-                                {json[el].map((arrElement: any) =>
-                                    typeof json[el] === 'object'
-                                        ? <ObjectBlock el={el}>{readObject(json[el])}</ObjectBlock>
-                                        : <Input value={arrElement} onChangeName={changeName} key={el+i} />,
-                                )}
-                            </ObjectBlock>
-                        </React.Fragment>
-                    );
+                    return <ObjectBlock el={'[] ' + el}>{renderArray(json[el])}</ObjectBlock>;
                 }
 
                 if (typeof json[el] === 'object') {
-                    return <ObjectBlock el={el}>{readObject(json[el])}</ObjectBlock>;
+                    return <ObjectBlock el={'{} ' + el}>{renderObject(json[el])}</ObjectBlock>;
                 };
 
-                return <Input name={el} value={json[el]} onChangeName={changeName} key={el+i} />;
+                return <Input name={el} value={json[el]} onChangeName={changeName} key={el + i} />;
             });
+        };
 
-        return readObject(JSON);
+        return renderObject(JSON);
     };
 
     return (
         <div className="jsonForm">
             <h1 className="mb-3">JSON Builder</h1>
-            <FontAwesomeIcon icon={faClone} />
-            <div className="input-group input-type-file mb-5">
-                <input type="file" className="form-control" id="inputGroupFile01"/>
+            <div className="d-flex flex-row-reverse">
+                <button type="button" className="mb-3 btn btn-primary" onClick={resetJson}>
+                    <FontAwesomeIcon icon={faRotateLeft} />
+                </button>
             </div>
             {renderJson()}
             <button
