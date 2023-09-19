c1def03b4eb5fe044cfb78154993f5d11edd8e0a
Jewgenij Larkin <jewglark@jewgenijs-macbook-pro.home>
2023-09-19 15:38:13 +0200

    reset map

--- src/modules/InitApplication/components/App/App.jsx
+++ src/modules/InitApplication/components/App/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { Map } from './Map';
+import logo from './logo.svg';
 import './style.scss';
 
 export const App = () => {
   return (
     <div className="app">
-      <div className="mapContainerWrapper">
-        <div className="mapContainer">
-          <Map/>
-        </div>
+      <div className="app-layout">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p className="text-secondary">Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="app-link" href="https://reactjs.org" target="_blank" rel="noreferrer">Learn React</a>
       </div>
     </div>
   );
